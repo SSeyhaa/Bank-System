afd6beb619aca9c9d851c9cd0fe334522fadd248
Seng Seyha <sengseyha2001@gmail.com>
2023-03-18 19:15:07 +0700

    finish CRUD of Admin

---
+++ src/main/java/com/ha/java/service/impl/AdminServiceImpl.java
@@ -0,0 +1,62 @@
+package com.ha.java.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.ha.java.dto.AdminDto;
+import com.ha.java.entity.Admin;
+import com.ha.java.mapper.AdminMapper;
+import com.ha.java.repository.AdminRepository;
+import com.ha.java.service.AdminService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class AdminServiceImpl implements AdminService {
+	
+	private final AdminRepository adminRepository;
+
+	@Override
+	public AdminDto postAdmin(AdminDto adminDto) {
+		
+		Admin admin = AdminMapper.INSTANCE.toAdmin(adminDto);
+		AdminDto getAdminDto = AdminMapper.INSTANCE.toAdminDto(adminRepository.save(admin));
+		
+		return getAdminDto;
+	}
+
+	@Override
+	public AdminDto getAdmin(Long id) {
+		
+		Admin admin = adminRepository.findById(id).orElse(null);
+		
+		return AdminMapper.INSTANCE.toAdminDto(admin);
+	}
+
+	@Override
+	public AdminDto putAdmin(Long id, AdminDto adminDto) {
+		
+		Admin admin = adminRepository.findById(id).orElse(null);
+		admin.setPassword(adminDto.getPassword());
+		admin.setUsername(adminDto.getUsername());
+		
+		AdminDto getAdminDto = AdminMapper.INSTANCE.toAdminDto(adminRepository.save(admin));
+		return getAdminDto;
+	}
+
+	@Override
+	public String deleteAdmin(Long id) {
+		
+		AdminDto adminDto = getAdmin(id);
+		Admin admin = AdminMapper.INSTANCE.toAdmin(adminDto);
+		if(admin != null) {
+			adminRepository.delete(admin);
+			return "Delete Successfully !";
+		}
+		else {
+			return "NOT EXIST";
+		}
+		
+	}
+
+}
